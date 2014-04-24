ecc47f8ad2248f5192110afe404f3d3b770d59e0
boysbee <gogorenger@gmail.com>
2014-04-25 00:18:32 +0700

    revise format code

--- src/main/java/my/spring/batch/processsor/PersonItemProcessor.java
+++ src/main/java/my/spring/batch/processsor/PersonItemProcessor.java
@@ -7,14 +7,14 @@ import org.springframework.batch.item.ItemProcessor;
 
 public class PersonItemProcessor implements ItemProcessor<Person, Person> {
 
-    public Person process(final Person person) {
-        final String firstName = person.getFirstName().toUpperCase();
-        final String lastName = person.getLastName().toUpperCase();
-        
-        final Person tranformedPerson = new Person(firstName, lastName);
+  public Person process(final Person person) {
+    final String firstName = person.getFirstName().toUpperCase();
+    final String lastName = person.getLastName().toUpperCase();
 
-        System.out.println("Converting ( " + person + " into "+  tranformedPerson);
-        
-        return tranformedPerson;
-	}
+    final Person tranformedPerson = new Person(firstName, lastName);
+
+    System.out.println("Converting ( " + person + " into " + tranformedPerson);
+
+    return tranformedPerson;
+  }
 }
